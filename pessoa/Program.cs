23031ec39cd927529e9f130b454d5c8d1c2d1278
Rawdrey <rawdrey@gmail.com>
2024-04-04 21:41:24 -0300

    programa dados pessoa

---
+++ pessoa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            pessoa1.Nome = "João";
+            pessoa1.Idade = 64;
+            pessoa1.Altura = 1.62;
+            pessoa1.Apresentar();
+        }
+    }
+}
---
+++ pessoa/pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pessoa
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public double Altura { get; set; }
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, idade: {Idade}, altura: {Altura}.");
+
+        }
+    }
+}
